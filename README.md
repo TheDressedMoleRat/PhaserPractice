afaecdce495e0ea7bcf0ee568bdf61ef2be026e0
Alvar <alvardepaulis@gmail.com>
2025-09-28 01:02:16 +0200

    added enemies and a death screen and explained the game in the readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PhaserPractice
-I want to learn the Phaser js library for making web games and here is the result hehe
+![game preview](game.png)
+I wanted to learn the JS framework Phaser for making web games, so I followed their tutorial called [Making your first Phaser Game](https://docs.phaser.io/phaser/getting-started/making-your-first-phaser-game). I followed it pretty closely, but I made sure to try and understand what I was doing, because I wanted to actually learn the framework :)
+
+You can play the little game I made on [the game page](https://thedressedmolerat.github.io/PhaserPractice/). The idea is that you (the kitty meow meow) eats all the sushi, but when you're done the sushi reappears but also evil spicy wasabi appears that you have to dodge.
+
+You can use WASD and arrow keys and space to play, and R to restart!
+
+I've used the [Phaser](https://phaser.io/) framwork for the rendering and the physics and the logic :3
--- game.js
+++ game.js
@@ -1,68 +1,141 @@
-class Example extends Phaser.Scene {
+class CatGame extends Phaser.Scene {
 	preload() {
 		this.load.image('cat', 'assets/cat.png');
 		this.load.image('background', 'assets/background.png');
 		this.load.image('platform', 'assets/platform.png');
 		this.load.image('sushi', 'assets/sushi.png');
+		this.load.image('wasabi', 'assets/wasabi.png');
 	}
 
 	create() {
+		////////////////// setup
+		this.keys = this.input.keyboard.addKeys({ 
+			'restart': Phaser.Input.Keyboard.KeyCodes.R,
+			'w': Phaser.Input.Keyboard.KeyCodes.W, 
+			's': Phaser.Input.Keyboard.KeyCodes.S,
+			'a': Phaser.Input.Keyboard.KeyCodes.A,
+			'd': Phaser.Input.Keyboard.KeyCodes.D,
+			'up': Phaser.Input.Keyboard.KeyCodes.UP,
+			'down': Phaser.Input.Keyboard.KeyCodes.DOWN,
+			'left': Phaser.Input.Keyboard.KeyCodes.LEFT,
+			'right': Phaser.Input.Keyboard.KeyCodes.RIGHT,
+			'space': Phaser.Input.Keyboard.KeyCodes.SPACE
+		});
+
 		this.add.image(640, 360, 'background');
 		
 		this.score = 0;
-		this.score_text = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+		this.level = 1;
+		this.score_text = this.add.text(16, 16, '', { fontSize: '32px', fill: '#000' });
+		this.updateText();
 		
+		////////////////// platforms
 		this.platforms = this.physics.add.staticGroup();
 
-		this.platforms.create(170, 223, 'platform');
 		this.platforms.create(218, 553, 'platform');
-		this.platforms.create(423, 387, 'platform');
-		this.platforms.create(650, 530, 'platform');
-		this.platforms.create(850, 380, 'platform');
+		this.platforms.create(380, 300, 'platform');
+		this.platforms.create(650, 500, 'platform');
+		this.platforms.create(890, 300, 'platform');
 		this.platforms.create(1050, 530, 'platform');
-		this.platforms.create(1115, 200, 'platform');
 
+		let floor = this.platforms.create(640, 690);
+		floor.setScale(100, 1);
+		floor.refreshBody();
+		floor.setVisible(false);
+
+		////////////////// player
 		this.player = this.physics.add.sprite(100, 100, 'cat');
-
+		this.player.setSize(90, 60);
+		this.player.setOffset(40, 30);
+		
 		this.player.setCollideWorldBounds(true);
-
 		this.physics.add.collider(this.player, this.platforms);
 
-		this.cursors = this.input.keyboard.createCursorKeys();
+		////////////////// sushis
+		this.spawnSushis();
 
-		this.sushis = this.physics.add.group({
-			key: 'sushi',
-			repeat: 11,
-			setXY: { x: 100, y: 0, stepX: 100 }
-		});
-
-		this.sushis.children.iterate(function (child) {
-			child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-		});
-
-		this.physics.add.collider(this.sushis, this.platforms);
-		this.physics.add.overlap(this.player, this.sushis, this.collectStar, null, this);
-	}
-
-	collectStar(player, star) {
-		star.disableBody(true, true);
-
-		this.score += 1;
-		this.score_text.setText('Score: ' + this.score);
+		////////////////// wasabis
+		this.wasabis = this.physics.add.group();
+		this.physics.add.collider(this.wasabis, this.platforms);
+		this.physics.add.collider(this.player, this.wasabis, this.hitwasabi, null, this);
 	}
 
 	update() {
-		if (this.cursors.left.isDown) {
+		if (this.keys['restart'].isDown) {
+			this.scene.restart();
+		}
+
+		if (this.keys['a'].isDown || this.keys['left'].isDown) {
 			this.player.body.setVelocityX(-300);
-		} else if (this.cursors.right.isDown) {
+		} else if (this.keys['d'].isDown || this.keys['right'].isDown) {
 			this.player.body.setVelocityX(300);
 		} else {
 			this.player.body.setVelocityX(0);
 		}
 
-		if (this.cursors.up.isDown && this.player.body.touching.down) {
-			this.player.setVelocityY(-800);
+		if (this.keys['w'].isDown || this.keys['space'].isDown || this.keys['up'].isDown) {
+			if (this.player.body.touching.down) {
+				this.player.setVelocityY(-1000);
+			}
 		}
+	}
+
+	spawnSushis() {
+		this.sushis = this.physics.add.group({
+			key: 'sushi',
+			repeat: 7,
+			setXY: { x: 100, y: 0, stepX: 150 }
+		});
+
+		this.sushis.children.iterate(function(sushi) {
+			let max_speed = 100
+			sushi.setBounceY(0.8);
+			sushi.setVelocity(Phaser.Math.Between(-max_speed, max_speed), Phaser.Math.Between(-max_speed, max_speed));
+			sushi.setCollideWorldBounds(true);
+		});
+
+		this.physics.add.collider(this.sushis, this.platforms);
+		this.physics.add.overlap(this.player, this.sushis, this.eatSushi, null, this);
+	}
+
+	eatSushi(player, sushi) {
+		sushi.disableBody(true, true);
+
+		this.score += 1;
+		this.updateText();
+
+		if (this.sushis.countActive(true) != 0) return;
+		
+		// if all sushis are eaten:
+		
+		this.level += 1;
+		this.updateText();
+
+		this.spawnSushis();
+
+		let wasabi_x = 0;
+		if (player.x > 640) {
+			wasabi_x = Phaser.Math.Between(0, 640);
+		} else {
+			wasabi_x = Phaser.Math.Between(640, 1280);
+		}
+
+		let wasabi = this.wasabis.create(wasabi_x, 20, 'wasabi');
+
+		wasabi.setBounce(1);
+		wasabi.setCollideWorldBounds(true);
+		wasabi.setVelocity((Math.random() > .5) ? -200 : 200, 20);
+	}
+
+	hitwasabi(player, wasabi) {
+		this.physics.pause();
+		player.setTint(0xff0000);
+		
+		this.score_text.setText(`You accidentally ate the wasabi and it was too spicy!\n[press R to play again]\nSushis: ${this.score}  Level: ${this.level}`);
+	}
+
+	updateText() {
+		this.score_text.setText(`Sushis: ${this.score}\nLevel: ${this.level}`);
 	}
 }
 
@@ -70,7 +143,7 @@ const config = {
 	type: Phaser.AUTO,
 	width: 1280,
 	height: 720,
-	scene: Example,
+	scene: CatGame,
 	physics: {
 		default: 'arcade',
 		arcade: {
